e9ad5041e34d3702a85d1df95ebcabf267282ceb
walnut1024 <>
2026-05-15 11:09:34 +0800

    Update Yeek to v2.0.0-alpha.15

--- Casks/yeek.rb
+++ Casks/yeek.rb
@@ -1,6 +1,6 @@
 cask "yeek" do
-  version "2.0.0-alpha.14"
-  sha256 "348cd5ea4cb8ae82f42f77755590295767743e2f33b97a9f262d31c7e9c77a8f"
+  version "2.0.0-alpha.15"
+  sha256 "3b02859ddea080c2a14d81460719e7b1768b38119dd3e37fc9c047b84ec2bd19"
 
   url "https://github.com/walnut1024/yeek/releases/download/v#{version}/Yeek_#{version}_aarch64.dmg"
   name "Yeek"
